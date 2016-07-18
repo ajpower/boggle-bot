c0251d42cb50b18c04825dee041a130677e75ec7
Alessandro Power <alessandrojpower@gmail.com>
2016-07-18 15:15:45 -0400

    Created Trie class.

--- boggle-solver/boggle.cpp
+++ boggle-solver/boggle.cpp
@@ -3,6 +3,9 @@
  */
 #include "boggle.hpp"
 
+/* Declare static data members. */
+Trie<bool> Boggle::trie;
+
 /* Constructors. */
 Boggle::Boggle(bpy::object board) {
 	for (std::size_t i = 0; i < 16; ++i) {
@@ -18,4 +21,9 @@ char Boggle::get(std::size_t i) const {
 /* Mutator functions. */
 void Boggle::set(std::size_t i, char c) {
 	board_[i] = c;
+}
+
+/* Static functions. */
+void Boggle::add_dictionary(const std::string& dictionary_path) {
+
 }
--- boggle-solver/boggle.hpp
+++ boggle-solver/boggle.hpp
@@ -6,41 +6,54 @@
 #include <array>
 #include <boost/python.hpp>
 
+#include "trie.hpp"
+
 namespace bpy = boost::python;
 
 /*
  * Class for representing and solving a 4x4 boggle board, meant to be used within Python.
  */
-// TODO len, str, docstrings.
+// TODO len, str, docstrings, make setter and getters 2D?
 class Boggle {
 public:
 	/* Constructors. */
 	/*
-	 * Creates a boggle board out of a Python container.
+	 * Create a boggle board out of a Python container.
 	 */
 	Boggle(bpy::object board);
 
 	/* Accessor functions. */
 	/*
-	 * Returns the ith char in the board. Called by __getitem__.
+	 * Return the ith char in the board. Called by __getitem__.
 	 */
 	// TODO implement exceptions, slices, negative indices.
 	char get(std::size_t i) const;
 
 	/* Mutator functions. */
 	/*
-	 * Sets the value of the ith char in the board. Called by _setitem__.
+	 * Set the value of the ith char in the board. Called by _setitem__.
 	 */
 	// TODO implement exceptions, slices, negative indices.
 	void set(std::size_t i, char c);
 
+	// Static functions.
+	/*
+	 * Add contents of dictionary to collection of legal words (implemented internally as a trie).
+	 */
+	static void add_dictionary(const std::string& dictionary_path);
+
 private:
 	/* Data members. */
 	std::array<char, 16> board_; // Boggle board.
+
+	/* Static data members. */
+	static Trie<bool> trie; // Trie containing all legal words.
 };
 
 BOOST_PYTHON_MODULE (boggle) {
 	bpy::class_<Boggle>("Boggle", bpy::init<bpy::object>())
 			.def("__getitem__", &Boggle::get)
-			.def("__setitem__", &Boggle::set);
+			.def("__setitem__", &Boggle::set)
+			.def("add_dictionary", &Boggle::add_dictionary)
+			.staticmethod("add_dictionary");
 }
