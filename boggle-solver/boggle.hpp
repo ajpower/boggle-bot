17ac9a532f6244875f0d380e253365ec6798c20e
Alessandro Power <alessandrojpower@gmail.com>
2016-08-01 14:59:09 -0400

    Added Boggle.solve.

--- boggle-solver/boggle.hpp
+++ boggle-solver/boggle.hpp
@@ -3,7 +3,10 @@
 #include <algorithm>
 #include <array>
 #include <fstream>
+#include <stack>
 #include <string>
+#include <vector>
+#include <boost/container/static_vector.hpp>
 
 #include "trie.hpp"
 
@@ -45,6 +48,56 @@ private:
 	static Trie trie; // Trie containing the words in a dictionary in uppercase letters.
 
 	/*
+	 * Lookup table to find the neighbours of a square on the Boggle board.
+	 */
+	class NeighbourTable {
+		friend Boggle;
+	public:
+		NeighbourTable() {
+			// Find the range of rows and columns constituting the neighbours of each Boggle square.
+			for (std::size_t i = 0; i < neighbours_.size(); ++i) {
+				std::size_t min_row, max_row, min_col, max_col;
+				min_row = i / M == 0 ? 0 : i / M - 1;
+				max_row = i / M == N - 1 ? N - 1 : i / M + 1;
+				min_col = i % M == 0 ? 0 : i % M - 1;
+				max_col = i % M == M - 1 ? M - 1 : i % M + 1;
+
+				for (std::size_t row = min_row; row <= max_row; ++row) {
+					for (std::size_t col = min_col; col <= max_col; ++col) {
+						if (row * M + col != i) {
+							neighbours_[i].push_back(row * M + col);
+						}
+					}
+				}
+			}
+		}
+
+		boost::container::static_vector<std::size_t, 8> operator[](std::size_t i) const {
+			return neighbours_[i];
+		}
+
+		boost::container::static_vector<std::size_t, 8>& operator[](std::size_t i) {
+			return neighbours_[i];
+		}
+
+	private:
+		std::array<boost::container::static_vector<std::size_t, 8>, N * M> neighbours_;
+	};
+
+	static const NeighbourTable neighbour_table;
+
+	/*
+	 * Find all words that start from the ith element of the Boggle board and place them in the
+	 * given vector. No bounds checks are made.
+	 */
+	void solve(std::size_t i, std::vector<std::string>& words) const;
+
+	/*
+	 * Return the word formed by following the given path through the Boggle board.
+	 */
+	std::string path2word(const std::vector<std::size_t>& path);
+
+	/*
 	 * Return true if string contains only ASCII letters.
 	 */
 	static bool ascii_word(const std::string& s);
@@ -53,6 +106,9 @@ private:
 /* Redeclaration of static data members. */
 template <std::size_t N, std::size_t M>
 Trie Boggle<N, M>::trie;
+
+template <std::size_t N, std::size_t M>
+const typename Boggle<N, M>::NeighbourTable Boggle<N, M>::neighbour_table;
 
 template <std::size_t N, std::size_t M>
 Boggle<N, M>::Boggle(const std::string& s) {
@@ -89,8 +145,54 @@ void Boggle<N, M>::load_dictionary(const std::string& file) {
 }
 
 template <std::size_t N, std::size_t M>
+void Boggle<N, M>::solve(std::size_t i, std::vector<std::string>& words) const {
+	//TODO explain algorithm
+	//TODO use Boost static/small vector
+	using path_t = std::vector<std::size_t>;
+
+	std::stack<path_t> paths;
+	paths.emplace(1, i);
+
+	while (not paths.empty()) {
+		path_t path = paths.top();
+		paths.pop();
+
+		std::string word = path2word(path);
+		if (not trie.has_prefix(word.c_str())) {
+			continue;
+		}
+		if (trie.has_string(word.c_str())) {
+			words.push_back(std::move(word));
+		}
+
+		std::size_t last_square = path.back();
+		auto neighbours = neighbour_table[last_square];
+		for (std::size_t neighbour : neighbours) {
+			if (std::find(path.begin(), path.end(), neighbour) == path.end()) {
+				path_t new_path = path;
+				new_path.push_back(neighbour);
+				paths.push(std::move(new_path));
+			}
+		}
+	}
+}
+
+template <std::size_t N, std::size_t M>
 bool Boggle<N, M>::ascii_word(const std::string& s) {
 	return std::all_of(s.begin(), s.end(), [](char c) {
 		return (c >= 'A' and c <= 'Z') or (c >= 'a' and c <= 'z');
 	});
+}
+
+template <std::size_t N, std::size_t M>
+std::string Boggle<N, M>::path2word(const std::vector<std::size_t>& path) {
+	std::string word;
+	for (auto i : path) {
+		char c = board_[i];
+		word.push_back(c);
+		if (c == 'Q') {
+			word.push_back('U');
+		}
+	}
+	return word;
 }
