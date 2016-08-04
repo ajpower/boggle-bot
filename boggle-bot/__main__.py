6ab0d791ee0b4f5bd0ec9fe936ce7b2f2068f41f
Alessandro Power <alessandrojpower@gmail.com>
2016-08-04 09:39:04 -0400

    Added username and password prompt, removed max puzzles.

--- boggle-bot/__main__.py
+++ boggle-bot/__main__.py
@@ -1,4 +1,5 @@
 """Main entry point of boggle-bot."""
+import getpass
 import os
 import sys
 import time
@@ -7,14 +8,16 @@ from boggle import Boggle
 from wordplays import Wordplays
 
 MAX_CONSECUTIVE_ERRORS = 3
-TOTAL_PUZZLES = 100
 
 module_dir = os.path.dirname(__file__)
 Boggle.load_dictionary(module_dir + '/dict.list')
 
+username = input('Enter username: ')
+password = getpass.getpass(prompt='Enter password: ')
+
 with Wordplays() as wp:
     try:
-        if not wp.login(username='Trump4Prez', password='qwerty123'):
+        if not wp.login(username=username, password=password):
             print('Login unsuccessful.', file=sys.stderr)
             print('Exiting.', file=sys.stderr)
             sys.exit(1)
@@ -26,18 +29,19 @@ with Wordplays() as wp:
     print('Login successful.\n')
 
     # Continuously solve puzzles until too many consecutive errors are
-    # encountered or the number of solved puzzles exceeds the provided limit.
+    # encountered.
     consecutive_errors = 0
     n_puzzles = 0
-    while (consecutive_errors < MAX_CONSECUTIVE_ERRORS
-           and n_puzzles < TOTAL_PUZZLES):
+    while consecutive_errors < MAX_CONSECUTIVE_ERRORS:
         try:
             boggle_elements = wp.start_boggle()
             b = Boggle(boggle_elements)
             words = b.solve()
             score, max_score = wp.solve(words)
+
             print('Puzzle number: {:5s}, score: {:>4s}, max score: {:>4s}'.
                   format(wp.pzlnbr, score, max_score))
+
             time.sleep(1)  # To avoid DOSing the server.
             consecutive_errors = 0
             n_puzzles += 1
@@ -45,8 +49,5 @@ with Wordplays() as wp:
             print('Error:', e, file=sys.stderr)
             consecutive_errors += 1
 
-    if consecutive_errors == MAX_CONSECUTIVE_ERRORS:
-        print('\nToo many consecutive errors were encountered. Exiting.',
-              file=sys.stderr)
-    else:
-        print('\n{} puzzles were solved successfully.'.format(TOTAL_PUZZLES))
+    print('Too many consecutive errors were encountered. Exiting.',
+          file=sys.stderr)
