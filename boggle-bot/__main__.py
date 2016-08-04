b75b564dbb1608130eae785209a82d634978cf6c
Alessandro Power <alessandrojpower@gmail.com>
2016-08-04 09:32:46 -0400

    Added max consecutive errors and max puzzles solved.

--- boggle-bot/__main__.py
+++ boggle-bot/__main__.py
@@ -6,8 +6,11 @@ import time
 from boggle import Boggle
 from wordplays import Wordplays
 
-dir = os.path.dirname(__file__)
-Boggle.load_dictionary(dir + '/dict.list')
+MAX_CONSECUTIVE_ERRORS = 3
+TOTAL_PUZZLES = 100
+
+module_dir = os.path.dirname(__file__)
+Boggle.load_dictionary(module_dir + '/dict.list')
 
 with Wordplays() as wp:
     try:
@@ -20,16 +23,30 @@ with Wordplays() as wp:
         print('Exiting.', file=sys.stderr)
         sys.exit(1)
 
-    print('Login successful.')
+    print('Login successful.\n')
 
-    while True:
+    # Continuously solve puzzles until too many consecutive errors are
+    # encountered or the number of solved puzzles exceeds the provided limit.
+    consecutive_errors = 0
+    n_puzzles = 0
+    while (consecutive_errors < MAX_CONSECUTIVE_ERRORS
+           and n_puzzles < TOTAL_PUZZLES):
         try:
             boggle_elements = wp.start_boggle()
             b = Boggle(boggle_elements)
             words = b.solve()
             score, max_score = wp.solve(words)
-            print('score: {}, max score: {}'.format(score, max_score))
-            time.sleep(1)
+            print('Puzzle number: {:5s}, score: {:>4s}, max score: {:>4s}'.
+                  format(wp.pzlnbr, score, max_score))
+            time.sleep(1)  # To avoid DOSing the server.
+            consecutive_errors = 0
+            n_puzzles += 1
         except Exception as e:
             print('Error:', e, file=sys.stderr)
-            print('Continuing anyways.', file=sys.stderr)
+            consecutive_errors += 1
+
+    if consecutive_errors == MAX_CONSECUTIVE_ERRORS:
+        print('\nToo many consecutive errors were encountered. Exiting.',
+              file=sys.stderr)
+    else:
+        print('\n{} puzzles were solved successfully.'.format(TOTAL_PUZZLES))
