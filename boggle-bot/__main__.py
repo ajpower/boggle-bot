9aa963d766083022f63d9dd23e14314df923b3e2
Alessandro Power <alessandrojpower@gmail.com>
2016-08-03 20:05:09 -0400

    Put boggle solver on infinite loop

--- boggle-bot/__main__.py
+++ boggle-bot/__main__.py
@@ -1,5 +1,6 @@
 """Main entry point of boggle-bot."""
 import sys
+import time
 
 from boggle import Boggle
 from wordplays import Wordplays
@@ -8,7 +9,7 @@ Boggle.load_dictionary('dict.list')
 
 with Wordplays() as wp:
     try:
-        if not wp.login(username='totally_human', password='qwerty123'):
+        if not wp.login(username='Trump4Prez', password='qwerty123'):
             print('Login unsuccessful.', file=sys.stderr)
             print('Exiting.', file=sys.stderr)
             sys.exit(1)
@@ -19,8 +20,14 @@ with Wordplays() as wp:
 
     print('Login successful.')
 
-    boggle_elements = wp.start_boggle()
-    b = Boggle(boggle_elements)
-    words = b.solve()
-    score, max_score = wp.solve(words)
-    print(score, max_score)
+    while True:
+        try:
+            boggle_elements = wp.start_boggle()
+            b = Boggle(boggle_elements)
+            words = b.solve()
+            score, max_score = wp.solve(words)
+            print('score: {}, max score: {}'.format(score, max_score))
+            time.sleep(1)
+        except Exception as e:
+            print('Error:', e, file=sys.stderr)
+            print('Continuing anyways.', file=sys.stderr)
